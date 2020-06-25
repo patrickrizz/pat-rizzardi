f57733bf0793a529b56b695d1de3b2b2abc00297
Patrick Rizzardi <patrick.d.rizzardi@gmail.com>
2020-06-25 09:44:52 -0400

    configure hanndlesessions

--- app/handleSession.js
+++ app/handleSession.js
@@ -2,21 +2,17 @@ const session = require('express-session');
 const { app } = require("../index");
 function handleSession() {
     if (app.get('env') === 'production') {
-        // app.use(session({
-        //     secret: [process.env.SESSION_SECRET, process.env.SESSION_SECRET_2],
-        //     name: 'awesomeSession',
-        //     resave: false,
-        //     saveUninitialized: false,
-        //     cookie: {
-        //         //secure: true,
-        //         //httpOnly: true,
-        //         //domain: 'patrizzardi.com',
-        //         maxAge: 60000
-        //     } //only used with https, can't with http. It secures the cookie, sameSite is for same site enforcment
-        // }));
         app.use(session({
-            secret: process.env.SESSION_SECRET,
-            cookie: {}
+            secret: [process.env.SESSION_SECRET, process.env.SESSION_SECRET_2],
+            name: 'awesomeSession',
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                //secure: true,
+                //httpOnly: true,
+                //domain: 'patrizzardi.com',
+                maxAge: 60000
+            } //only used with https, can't with http. It secures the cookie, sameSite is for same site enforcment
         }));
     }
     else
