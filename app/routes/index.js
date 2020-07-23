88fb6ee53d90c2c5ed0bcc220b7a276d5f47aad2
Patrick Rizzardi <patrick.d.rizzardi@gmail.com>
2020-07-23 15:28:06 -0400

    changed github action and added blog section to index

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,31 +2,47 @@ const express = require('express')
 const { projects } = require("../../public/projects")
 const { jobs } = require("../../public/jobs")
 const { indexNav } = require("../../public/nav")
+const { api } = require("../../api/handlePosts")
 const router = express.Router()
 
 router.use(express.static('public'))
 
 //landing page
 router.get('/', (req, res) => {
-    res.render('index', {
-        projects: projects,
-        jobs: jobs,
-        nav: indexNav,
-        logo: "images/icons/logo.png"
+    api.posts.browse({
+        include: 'authors,tags',
+        limit: 'all',
+    }).then( posts => {
+        res.render('index', {
+            projects: projects,
+            jobs: jobs,
+            nav: indexNav,
+            logo: "images/icons/logo.png",
+            posts: posts
+        })
+        for (i = 0; i < posts.length; i++) {
+            if (posts[i].featured == true) {
+            console.log(posts[i].tags.name)
+            }
+        }
+        
     })
+
+
 })
+
 
 router.get('/projects/:projectId', (req, res) => {
     let slug = req.params['projectId'];
 
-    let project = projects.filter( p => { 
+    let project = projects.filter(p => {
         return p.slug == slug;
     });
 
     // do something if project is non-existant
     if (!project.length) {
         res.status(404).send('<h1 style="text-align: center font-size: 3.5em">404 Reqeust Not Found</h1>')
-        return; 
+        return;
     }
 
     res.render('projects', {
