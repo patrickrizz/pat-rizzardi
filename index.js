12459f18f53feda58bbadfdf0d08d0d3fc23c745
Patrick Rizzardi <patrick.d.rizzardi@gmail.com>
2020-08-05 09:23:52 -0400

    remove headers

--- index.js
+++ index.js
@@ -10,8 +10,18 @@ const { startServer } = require("./app/startServer")
 const { handleSession } = require("./app/handleSession")
 require('./config/passport-setup')
 
+const removeHeaders = () => {
+    app.use(function (req, res, next) {
+       res.removeHeader('X-Powered-By')
+       res.removeHeader('Server')
+
+       next()
+    })
+} 
+
 const handleMiddleware = () => {
     
+    removeHeaders()
     handleSession()
     app.use(passport.initialize())
     app.use(passport.session())
