1f5e01e37b9574546f57a03be93f5b80b66ac89a
Patrick Rizzardi <patrick.d.rizzardi@gmail.com>
2020-08-16 09:16:43 -0400

    a

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 module.exports = {
     development: {
         dialect: "sqlite",
-        storage: "./db/dev.sq",
+        storage: "./db/dev.sqlite",
         logging: false
     },
     test: {
@@ -12,7 +12,7 @@ module.exports = {
     },
     production: {
         dialect: "sqlite",
-        storage: "./db/production.sq",
+        storage: "./db/production.sqlite",
         logging: function (msg) {
             let log = fs.createWriteStream('./log/database.txt', { flags: 'a' }, (err) => {
                 if (err) console.log(err)
