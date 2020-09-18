d62dbe34c35b8059d8ac0f9c42df71561fe99d8a
Patrick Rizzardi <patrick.d.rizzardi@gmail.com>
2020-09-17 23:12:47 -0400

    added unmix qa

--- test.js
+++ test.js
@@ -1,32 +1,136 @@
 let tests = (a, b, c) => {
+    console.log('------------------------')
+    console.log('Sentence unscramble test')
+    console.log('------------------------')
+
     a_expected = "215364"
     b_expected = "This is a mixed up string."
-    c_expected = "abcde"    
+    c_expected = "abcde"
 
-    if (a == a_expected) {
-        console.log(`${a} Passed`)
-    } else {
-        console.log(`Test a equals ${a} but should equal '${a_expected}'`)
-    }
+    if (a === a_expected) {
+        console.log(`1. Passed, ${a}`)
+    } else console.log(`1. Test a equals ${a} but should equal '${a_expected}'`)
 
-    if (b == b_expected) {
-        console.log(`${b} Passed`)
-    } else {
-        console.log(`Test a equals ${b} but should equal '${b_expected}'`)
-    }
+    if (b === b_expected) {
+        console.log(`2. Passed, ${b}`)
+    } else console.log(`2. Test a equals ${b} but should equal '${b_expected}'`)
 
-    if (c == c_expected) {
-        console.log(`${c} Passed`)
-    } else {
-        console.log(`Test a equals ${c} but should equal '${c_expected}'`)
-    }
+    if (c === c_expected) {
+        console.log(`3. Passed, ${c}`)
+    } else console.log(`3. Test a equals ${c} but should equal '${c_expected}'`)
 }
 
-let pTests = (str, str2, str3) => {
-    console.log(str)
-    console.log(str2)
-    console.log(str3)
+let bracketTests = (str, str2, str3, str4, str5, str6) => {
+    console.log('------------------------')
+    console.log('Bracket Matcher')
+    console.log('------------------------')
+
+    let str_expected = false
+    let str2_expected = false
+    let str3_expected = true
+    let str4_expected = true
+    let str5_expected = false
+    let str6_expected = true
+
+    if (str === str_expected) {
+        console.log(`1. Passed, ${str}`)
+    } else console.log(`1.  Failed, expected answer: ${str_expected}`)
+
+    if (str2 === str2_expected) {
+        console.log(`2. Passed, ${str2}`)
+    } else console.log(`2. Failed, expected answer: ${str2_expected}`)
+
+    if (str3 === str3_expected) {
+        console.log(`3. Passed, ${str3}`)
+    } else console.log(`3. Failed, expected answer: ${str3_expected}`)
+
+    if (str4 === str4_expected) {
+        console.log(`4. Passed, ${str4}`)
+    } else console.log(`4. Failed, expected answer: ${str4_expected}`)
+
+    if (str5 === str5_expected) {
+        console.log(`5. Passed, ${str5}`)
+    } else console.log(`5. Failed, expected answer: ${str5_expected}`)
+
+    if (str6 === str6_expected) {
+        console.log(`6. Passed, ${str6}`)
+    } else console.log(`6. Failed, expected answer: ${str6_expected}`)
+
+}
+
+let packageTests = (t1, t2, t3, t4) => {
+    console.log('------------------------')
+    console.log('How many small bags')
+    console.log('------------------------')
+
+    t1_expected = 5
+    t2_expected = 10
+    t3_expected = -1
+    t4_expected = 0
+
+    if (t1 === t1_expected) {
+        console.log(`1. Passed, ${t1} small bags`)
+    } else console.log(`1.  Failed, expected answer: ${t1_expected} small bags`)
+
+    if (t2 === t2_expected) {
+        console.log(`2. Passed, ${t2} small bags`)
+    } else console.log(`2. Failed, expected answer: ${t2_expected} small bags`)
+
+    if (t3 === t3_expected) {
+        console.log(`3. Passed, ${t3} small bags`)
+    } else console.log(`3. Failed, expected answer: ${t3_expected} small bags`)
+
+    if (t4 === t4_expected) {
+        console.log(`4. Passed, ${t4} small bags`)
+    } else console.log(`4. Failed, expected answer: ${t4_expected} small bags`)
+}
+
+let threatTest = (t1, t2, t3) => {
+    console.log('------------------------')
+    console.log('Does the array have consecutive numbers')
+    console.log('------------------------')
+
+    t1_expected = 'Has consecutive numbers'
+    t2_expected = 'Does not have consecutive numbers'
+    t3_expected = 'Has consecutive numbers'
+
+    if (t1 === t1_expected) {
+        console.log(`1. Passed, ${t1}`)
+    } else console.log(`1. Failed, expected answer: ${t1_expected}`)
+
+    if (t2 === t2_expected) {
+        console.log(`2. Passed, ${t2}`)
+    } else console.log(`2. Failed, expected answer: ${t2_expected}`)
+
+    if (t3 === t3_expected) {
+        console.log(`3. Passed, ${t3}`)
+    } else console.log(`3. Failed, expected answer: ${t3_expected}`)
+}
+
+let objectProperties = (t1, t2, t3) => {
+    console.log('------------------------')
+    console.log('Are all properties equal')
+    console.log('------------------------')
+
+    t1_expected = true
+    t2_expected = true
+    t3_expected = false
+
+    if (t1 === t1_expected) {
+        console.log(`1. Passed, ${t1}`)
+    } else console.log(`1. Failed, expected answer: ${t1_expected}, your answer: ${t1}`)
+
+    if (t2 === t2_expected) {
+        console.log(`2. Passed, ${t2}`)
+    } else console.log(`2. Failed, expected answer: ${t2_expected}, your answer: ${t2}`)
+
+    if (t3 === t3_expected) {
+        console.log(`3. Passed, ${t3}`)
+    } else console.log(`3. Failed, expected answer: ${t3_expected}, your answer: ${t3}`)
 }
 
 exports.tests = tests
-exports.pTests = pTests
+exports.bracketTests = bracketTests
+exports.packageTests = packageTests
+exports.threatTest = threatTest
+exports.objectProperties = objectProperties
--- unmix.js
+++ unmix.js
@@ -1,3 +1,4 @@
+const { vision_v1p1beta1 } = require('googleapis')
 const tests = require('./test.js')
 
 function unmix(str) {
@@ -20,17 +21,21 @@ let b = unmix("hTsii  s aimex dpus rtni.g") //this is a mixed up string
 
 let c = unmix("badce") //abcde
 
-tests.tests(a,b,c)
+tests.tests(a, b, c)
 
-let isParenthesizeBalance = (str) => {
+let bracketMatcher = (str) => {
     let stack = [];
 
     let open = {
-        '(': ')'
+        '(': ')',
+        '[': ']',
+        '{': '}'
     };
 
     let closed = {
-        ')': true
+        ')': true,
+        ']': true,
+        '}': true
     }
 
     for (let i = 0; i < str.length; i++) {
@@ -46,10 +51,105 @@ let isParenthesizeBalance = (str) => {
     return stack.length === 0;
 }
 
-//parenthesize tests
+//bracket tests
 
-let str = isParenthesizeBalance(')(())(') //not balanced
-let str2 = isParenthesizeBalance('(hello( (world)') //not balanced
-let str3 = isParenthesizeBalance('(hello (world))') //balanced
+let str = bracketMatcher(')(())(') //not balanced
+let str2 = bracketMatcher('(hello( (world)') //not balanced
+let str3 = bracketMatcher('(hello (world))') //balanced
 
-tests.pTests(str,str2,str3)
+let str4 = bracketMatcher('(hello [world])') //balanced
+let str5 = bracketMatcher('if (str.length % 2 === 1 newstr += str[str.length - 1]') //balanced
+let str6 = bracketMatcher('if (open[char]) { stack.push(char); } else if (closed[char]) { if (open[stack.pop()] !== char) return false; }') //balanced
+
+tests.bracketTests(str, str2, str3, str4, str5, str6)
+
+// We need to deliver a package of cookie bags. You will be given an inventory of small bags (1 kilo each) and big bags (5 kilos each), 
+// along with the goal amount of kilos  that we need to ship to the customer. Return the amount of small bags the package will contain, 
+// assuming we always use big bags first. Return -1 if it cannot be done.
+
+function createPackage(small, big, goal) {
+    //set bag weights
+    const BIGWEIGHT = 5
+    const SMALLWEIGHT = 1
+
+    //ensure goal is at least 1 otherwise return -1
+    if (goal < 1) return -1
+
+    //find number of big bags
+    let numOfBigBags = Math.floor(goal / BIGWEIGHT)
+
+    //check if you have enough big bags in inventory
+    if (numOfBigBags > big) numOfBigBags = big
+
+    //find number of small bags
+    let numOfSmallBags = ((goal - (numOfBigBags * BIGWEIGHT)) / SMALLWEIGHT)
+
+    //check if you have enough small bags in inventory
+    if (numOfSmallBags > small) return -1
+
+    //return answer
+    return numOfSmallBags
+}
+
+let package1 = createPackage(20, 1, 10)
+let package2 = createPackage(15, 1, 15)
+let package3 = createPackage(10, 10, -1)
+let package4 = createPackage(10, 2, 5)
+tests.packageTests(package1, package2, package3, package4)
+
+// Return 1 if the array contains three increasing consecutive numbers (e.g. [..., 4,5,6 ...], [...23,24,25,...]). Otherwise return 0.
+
+function tripleThreat(a) {
+    //first, second, third index must equal first index
+    let test = (a) => a[0] == a[1] - 1 && a[0] == a[2] - 2
+
+    //loop through each index
+    for (i = 0; i < a.length; i++) {
+
+        //run the test (true or false) through 3 indexs at a time
+        if (test(a.slice(i, i + 3))) {
+            //return answer if true
+            return 'Has consecutive numbers'
+        }
+    }
+    //return answer if false
+    return 'Does not have consecutive numbers'
+}
+
+let threat1 = tripleThreat([1, 3, 2, 3, 4, 5, 1])
+let threat2 = tripleThreat([55, 66, 55, 2000])
+let threat3 = tripleThreat([10, 10, -1, 0, 1])
+
+tests.threatTest(threat1, threat2, threat3)
+
+//Given the following sampleTree object, write a function leavesAreEqual which returns true if all values are identical, otherwise returns false
+
+function leavesAreEqual(obj) {
+    //determine if obj exists
+    if (!obj) return 'No object present'
+
+    let res = {};
+    function recurse(obj) {
+        for (let key in obj) {
+            let value = obj[key];
+            if (value != undefined) {
+                if (value && typeof value === 'object') {
+                    recurse(value, key);
+                } else {
+                    // Do your stuff here to var value
+                    res[key] = value;
+                }
+            }
+        }
+    }
+    recurse(obj);
+    let arr = Object.values(res)
+    arr = arr.reduce((a, b) => { return (a === b) ? a : false })
+    return (arr === false) ? false : true
+}
+
+let obj1 = leavesAreEqual({ a: 1, b: 1, c: { c1: 1, c2: 1 } })
+let obj2 = leavesAreEqual({ a: '1', b: '1', c: { c1: '1', c2: '1' } })
+let obj3 = leavesAreEqual({ a: 1, b: 1, c: { c1: 2, c2: 1 } })
+
+tests.objectProperties(obj1, obj2, obj3)
